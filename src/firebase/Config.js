28fd82fcea19d53968e89f822e75b082df44ce7e
dneog <debashisneog23@gmail.com>
2023-06-21 17:13:22 +0530

    sign-up-page

---
+++ src/firebase/Config.js
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4Ttgh67VY0tUS-PC7fyC72o43YoVKv4Q",
+  authDomain: "mail-box-client-5c3c3.firebaseapp.com",
+  projectId: "mail-box-client-5c3c3",
+  storageBucket: "mail-box-client-5c3c3.appspot.com",
+  messagingSenderId: "1001289830600",
+  appId: "1:1001289830600:web:12d90a7638c76c62e055fd"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const auth= getAuth(app);
+export const db= getFirestore(app);
+export const storage= getStorage(app);
+export default app;
---
+++ src/pages/Auth/SignUp.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Link, useNavigate } from 'react-router-dom';
+import {auth} from '../../firebase/Config';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+const SignUp = () => {
+    const navigate= useNavigate()
+    const [email, setEmail]= useState('');
+    const [password, setPassword]= useState('');
+    const [confirmPassword, setConfirmPassword]= useState('');
+
+    const handleSubmit=(e)=> {
+        e.preventDefault();
+        if(password !== confirmPassword){
+            alert('Password Dont match')
+        }
+
+        createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+   
+    const user = userCredential.user;
+  
+   
+     navigate('/login')
+   
+  })
+  .catch((error) => {
+    console.log(error.message);
+   
+  });
+    }
+
+  return (
+    <div className='w-25 m-auto mt-5'>
+        <div className='pt-5'>
+        <Form onSubmit={handleSubmit} className='border py-4 px-3' >
+        <h4 className='text-center'>Sign Up</h4>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e)=> setEmail(e.target.value)} />
+       
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" value={password} onChange={(e)=> setPassword(e.target.value)}  placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+       
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e)=> setConfirmPassword(e.target.value)}  />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+       
+      </Form.Group>
+      <div className='text-center'>
+      <Button  variant="primary" type="submit">
+        Submit
+      </Button>
+      </div>
+      
+    </Form>
+        </div>
+      <p className='text-center'>Already have an account ?  <Link to={'/login'}>
+        Login
+      </Link> </p>
+     
+
+    </div>
+  )
+}
+
+export default SignUp
